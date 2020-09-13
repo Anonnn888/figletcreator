39eecf6758e10889e9e44c196a03a3a0a4148b8c
Anon555 <70950457+Anonnn888@users.noreply.github.com>
2020-09-14 06:33:38 +0700

    Add files via upload

---
+++ figlet.sh
@@ -0,0 +1,24 @@
+#
+#
+#
+#
+clear
+figlet Figlet
+figlet Creator
+read -p "Masukan Text Untuk Dibuat Figlet >>" figl;
+sleep 1
+echo "Loading........" | lolcat
+sleep 0.1
+clear
+sleep 0.1
+echo "Sedang Membuat......" | lolcat
+sleep 0.4
+echo "Sabar....." | lolcat
+sleep 0.1
+echo "Selesai✓" | lolcat
+sleep 0.3
+figlet $figl | lolcat
+echo "Terimakasih" | lolcat
+sleep 0.5
+echo " By Anon555" | lolcat
+sleep 0.1
